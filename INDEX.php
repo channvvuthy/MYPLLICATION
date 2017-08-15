e41a41f54a0583efd53e06b63fb4ebf08b6dd049
channvvuthy <vuthychan271@yahoo.com>
2017-08-15 13:19:45 +0700

    add index.php

---
+++ INDEX.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Development</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ratione ipsa harum sunt, aperiam, assumenda ipsum earum, saepe sit eius consequuntur iure quasi impedit? Veritatis rem numquam nam harum tempore. </p>
+</body>
+</html>
